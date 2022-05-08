355ae46baff71e4a1482dfe0c84b554bd1f10ba3
Salar Hosseini <salar_hosseini11@yahoo.com>
2022-05-08 14:35:01 +0430

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { Container, Box, Heading } from '@chakra-ui/react';
+
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="yellow" p={3} mb={6} align="center">
+        Hello, i am a freaking front-end devepoler from tehran !
+      </Box>
+      <Box display={{ md:'felx'}}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Slrhoseini
+          </Heading>
+          <p>Artist, Developer, Producer and also a Student</p>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
+
+export default Page
